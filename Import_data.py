fdf546e3afcba8ea12ba81079d38d07e3de34ea4
Seb Budd <sebastianbudd@btinternet.com>
2020-08-22 13:13:21 +0100

    add new file

--- Import_data.py
+++ Import_data.py
@@ -7,3 +7,9 @@ ratings = pd.read_csv('ml-latest-small/ratings.csv')
 tags = pd.read_csv('ml-latest-small/tags.csv')
 
 print(links, movies, ratings, tags)
+
+n_users = len(ratings.userId.unique())
+print('n_users =', n_users)
+
+n_unique_tags = len(tags.tag.unique())
+print('n_unique_tags =', n_unique_tags)
